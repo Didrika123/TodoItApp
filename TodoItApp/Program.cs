b0ff8ecf51a2c45fe8b32eaa57943978f9fe41b3
didrika@protonmail.com <didrika@protonmail.com>
2020-09-29 04:48:06 -0700

    Add project files.

---
+++ TodoItApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoItApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
